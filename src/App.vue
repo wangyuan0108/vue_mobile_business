defd4b426825d69b1bc924dda88905866f512530
wangyuan0108 <870147852@qq.com>
2018-08-09 18:06:54 +0800

    feat(添加vantUI): 有赞ui库使用
    使用第三方ui组件和按需加载的配置

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <van-button type="primary">主要按钮</van-button>
     <router-view/>
   </div>
 </template>
 
 <script>
+import { Button } from 'vant';
+
 export default {
-  name: 'App'
-}
+    name: 'App',
+    components: {
+        vanButton: Button
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>

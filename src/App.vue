98366bda0bd097e805918422db078753228db327
wangyuan0108 <870147852@qq.com>
2018-08-13 18:05:33 +0800

    feat(banner轮播图): banner
    添加首页的轮播图

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <van-button type="primary">主要按钮</van-button>
-    <router-view/>
-  </div>
+    <div id="app">
+        <router-view/>
+    </div>
 </template>
 
 <script>
-import { Button } from 'vant';
-
 export default {
-    name: 'App',
-    components: {
-        vanButton: Button
-    }
+    name: 'App'
 };
 </script>
 
 <style>
-#app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-}
 </style>
---
+++ src/components/ShoppingMall.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <!--search bar layout-->
+        <div class="search-bar">
+            <van-row gutter="5">
+                <van-col span="3"><img :src="locationIcon" width="60%" class="location-icon" /></van-col>
+                <van-col span="16">
+                    <input type="text" class="search-input" />
+                </van-col>
+                <van-col span="5">
+                    <van-button size="mini">查找</van-button>
+                </van-col>
+
+            </van-row>
+        </div>
+        <!--swipwer area-->
+        <div class="swiper-area">
+            <van-swipe :autoplay="2000">
+                <van-swipe-item v-for="(banner,index) in bannerPicArray" :key="index">
+                    <img v-lazy="banner.imageUrl" width="100%" />
+                </van-swipe-item>
+            </van-swipe>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            locationIcon: require('@/assets/images/icon-address.png'),
+            bannerPicArray: [
+                { imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg' },
+                { imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg' },
+                { imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg' }
+            ]
+        };
+    }
+};
+</script>
+<style>
+.search-bar {
+    height: 2.2rem;
+    background-color: #e5017d;
+    line-height: 2.2rem;
+}
+
+.search-input {
+    width: 100%;
+    height: 1.3rem;
+    border-top: 0px;
+    border-left: 0px;
+    border-right: 0px;
+    border-bottom: 1px solid 1px !important ;
+    background-color: #e5017d;
+    color: #fff;
+}
+.location-icon {
+    padding-top: 0.3rem;
+    padding-left: 0.3rem;
+}
+.swiper-area {
+    width: 20rem;
+    clear: both;
+    text-align: center;
+}
+img[lazy='loading'] {
+    width: 4rem;
+    height: 4rem;
+    margin-top: 1rem;
+}
+</style>
